9f4ced925ded8da857f6de4bd398c7ac2d901f59
titi.ea <titi.cha@energyabsolute.co.th>
2024-05-13 14:51:25 +0700

    implement gRPC server and services

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"server/services"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
+	s := grpc.NewServer()
+
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	services.RegisterCalculatorServer(s, services.NewCalculatorServer())
+
+	fmt.Println("gRPC server listening on port 50051")
+	err = s.Serve(listener)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ services/calculator_server.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	context "context"
+	"fmt"
+)
+
+type calculatorServer struct{}
+
+func NewCalculatorServer() CalculatorServer {
+	return calculatorServer{}
+}
+
+func (calculatorServer) Hello(ctx context.Context, req *HelloRequest) (*HelloResponse, error) {
+	result := fmt.Sprintf("Hello %v", req.Name)
+	res := HelloResponse{
+		Result: result,
+	}
+
+	return &res, nil
+}
+
+func (calculatorServer) mustEmbedUnimplementedCalculatorServer() {}
